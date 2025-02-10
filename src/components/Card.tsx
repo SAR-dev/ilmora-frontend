008a8d3e28604ab97cd0ddfd05e6c76881ba3540
Sayed Rafi <sayedarrafi.bd@gmail.com>
2025-02-10 22:50:29 +0900

    up

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,9 +12,9 @@ const Card = ({
   children: ReactNode | ReactNode[]
 }) => {
   return (
-    <div className="card divide-y divide-base-300 border border-base-300 bg-base-100">
-      <div className="p-5 w-full flex justify-between items-center bg-info/20">
-        <div className="flex items-center gap-2 font-semibold text-info">
+    <div className="card divide-y divide-base-300 border border-base-300 bg-base-100 overflow-hidden">
+      <div className="p-5 w-full flex justify-between items-center bg-accent/20">
+        <div className="flex items-center gap-2 font-semibold text-accent">
           {headerIcon}
           {headerTitle}
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,9 @@ import Drawer from 'react-modern-drawer';
 import { Link } from "react-router";
 import { FiShoppingBag, FiShoppingCart } from "react-icons/fi";
 import { RxExit } from "react-icons/rx";
-import { FaMoon, FaSun } from "react-icons/fa";
-import { ThemeName, useThemeStore } from "stores/themeStore";
+import { FaMoon } from "react-icons/fa";
 
 const Navbar = () => {
-    const { theme, setTheme } = useThemeStore()
-    const toogleTheme = () => {
-        console.log(theme == ThemeName.Dark)
-        console.log(theme, ThemeName.Dark)
-        if (theme == ThemeName.Dark) setTheme(ThemeName.Light)
-        if (theme == ThemeName.Light) setTheme(ThemeName.Dark)
-    }
-
     const [showDrawer, setShowDrawer] = useState<boolean>(false)
     const toggleDrawer = () => {
         setShowDrawer((prevState) => !prevState)
@@ -31,12 +22,8 @@ const Navbar = () => {
             <div className="navbar-center">
             </div>
             <div className="navbar-end gap-1">
-                <button className="btn btn-circle" onClick={toogleTheme}>
-                    {theme == ThemeName.Dark ? (
-                        <FaMoon className="size-5" />
-                    ) : (
-                        <FaSun className="size-5" />
-                    )}
+                <button className="btn btn-circle">
+                    <FaMoon className="size-5" />
                 </button>
                 <button className="btn gap-2" onClick={toggleDrawer}>
                     <img
