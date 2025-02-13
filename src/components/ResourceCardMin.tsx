1d737c63a75a7d6432adc4c761d72b8f56c16523
Sayed Rafi <sayedarrafi.bd@gmail.com>
2025-02-13 13:22:52 +0900

    Update ResourceCardMin.tsx

--- src/components/ResourceCardMin.tsx
+++ src/components/ResourceCardMin.tsx
@@ -6,9 +6,11 @@ const ResourceCardMin = ({data}:{data: ResourcesResponse}) => {
     return (
         <div className="px-5 py-2 hover:bg-accent/20 transition duration-150 ease-in-out">
             <div className="flex items-center justify-between">
+                <div className="tooltip" data-tip={data.title}>
                 <div className="flex items-center gap-2">
                     <CiFileOn className="size-5" />
-                    <div>{data.title}</div>
+                    <div className="line-clamp-1">{data.title}</div>
+                </div>
                 </div>
                 <a href={data.link} target="_blank" className="btn btn-sm">
                     <VscLink className="size-4" />
